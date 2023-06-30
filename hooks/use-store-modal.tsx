712471be174bce2b0e2085025a69426d1faa8139
EugeniBykovski <bykovskieugeni@mail.ru>
2023-06-30 14:33:15 +0200

    added modal hook, store modal and sign up/in pages

---
+++ hooks/use-store-modal.tsx
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+interface useStoreModalStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+export const useStoreModal = create<useStoreModalStore>((set) => ({
+  isOpen: true,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
