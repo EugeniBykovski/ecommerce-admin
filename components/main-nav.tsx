84cd526895a5d2e66f8aff5a509eca5d690b25f1
EugeniBykovski <bykovskieugeni@mail.ru>
2023-07-05 14:54:19 +0200

    added navbar and store-switcher components

---
+++ components/main-nav.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+
+export function MainNav({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) {
+  const pathname = usePathname();
+  const params = useParams();
+
+  const routes = [
+    {
+      href: `/${params.storeId}/settings`,
+      label: "Settings",
+      active: pathname === `/${params.storeId}/settings`,
+    },
+  ];
+
+  return (
+    <nav
+      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
+      {...props}
+    >
+      {routes.map(({ href, label, active }) => (
+        <Link
+          key={href}
+          href={href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            active ? "text-black dark:text-white" : "text-muted-foreground"
+          )}
+        >
+          {label}
+        </Link>
+      ))}
+    </nav>
+  );
+}
