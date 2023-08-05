6e728e41175ef8177f81970101356e1f4f0bcb50
EugeniBykovski <bykovskieugeni@mail.ru>
2023-08-05 15:45:00 +0200

    created image upload module, added billboards entity for admin panel

---
+++ app/(dashboard)/[storeid]/(routes)/billboards/[billboardId]/page.tsx
@@ -0,0 +1,24 @@
+import prismadb from "@/lib/prismadb";
+import { BillboardForm } from "./components/billboard-form";
+
+const BillboardPage = async ({
+  params,
+}: {
+  params: { billboardId: string };
+}) => {
+  const billboard = await prismadb.billboard.findUnique({
+    where: {
+      id: params.billboardId,
+    },
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 p-4 py-0">
+        <BillboardForm initialData={billboard} />
+      </div>
+    </div>
+  );
+};
+
+export default BillboardPage;
--- app/(dashboard)/[storeid]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeid]/(routes)/settings/components/settings-form.tsx
@@ -14,6 +14,7 @@ import {
 import { Heading } from "@/components/ui/heading";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import { useOrigin } from "@/hooks/use-origin";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client";
 import axios from "axios";
@@ -37,6 +38,7 @@ interface SettingsFormProps {
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   const params = useParams();
   const router = useRouter();
+  const origin = useOrigin();
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
