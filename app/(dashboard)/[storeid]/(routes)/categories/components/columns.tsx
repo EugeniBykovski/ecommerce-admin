442557f33bc717eeff688eed341898a6175ccd8f
EugeniBykovski <bykovskieugeni@mail.ru>
2023-08-09 18:36:17 +0200

    added categories entity

---
+++ app/(dashboard)/[storeid]/(routes)/categories/components/columns.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+import { CellAction } from "./cell-action";
+
+export type CategoryColumn = {
+  id: string;
+  name: string;
+  billboardLabel: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<CategoryColumn>[] = [
+  {
+    accessorKey: "Name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "billboard",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Billboard
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => row.original.billboardLabel,
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
